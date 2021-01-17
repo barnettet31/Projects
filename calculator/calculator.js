59bd5120f543b4f304e034d2b83bf724819cc823
Travis <barnette.travis31@gmail.com>
2021-01-17 16:49:51 -0500

    using if statements to append the button values to the text area

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,15 @@
-var buttonClicked=null;
+var buttonClicked = null;
 
-document.querySelector(".button-div").addEventListener("click", function(event){
-  buttonClicked=event.target.value;
+document.querySelector(".button-div").addEventListener("click", function(event) {
+  buttonClicked = event.target.value;
+  var textArea = document.querySelector("textarea");
   console.log(buttonClicked);
-  if(buttonClicked==undefined){null}else{document.querySelector("textarea").innerHTML=buttonClicked;}
+  if (buttonClicked == 0 || buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9) {
+    textArea.append(buttonClicked);
+  } else if (buttonClicked == "clear") {
+    textArea.value = null;
+  } else {
+    null;
+  }
 
 });
