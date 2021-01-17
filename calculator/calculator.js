c206912045bd61ee68ee733f8a1f67e84946dbb1
Travis <barnette.travis31@gmail.com>
2021-01-17 16:28:04 -0500

    finally got it to add the button value to the text area

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,1 +1,8 @@
+var buttonClicked=null;
 
+document.querySelector(".button-div").addEventListener("click", function(event){
+  buttonClicked=event.target.value;
+  console.log(buttonClicked);
+  if(buttonClicked==undefined){null}else{document.querySelector("textarea").innerHTML=buttonClicked;}
+
+});
