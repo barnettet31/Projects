7c6e15ff497751f0c93ada02d299cf8a922b4444
Travis <barnette.travis31@gmail.com>
2021-01-17 21:22:46 -0500

    fixed up some styling on the equal sign and got away from regex because it was wrong

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,13 +1,12 @@
 var buttonClicked = null;
 var textAreaValue = 0;
-  var digit=/([0-9]|[(-/])/g;
+
 //This is a function that detects the button press and appends the inner text of the text-area div based on that input, then if the user clicks on the equal sign, it will run eval on the string.
 document.querySelector(".button-div").addEventListener("click", function(event) {
   buttonClicked = event.target.value;
   var textArea = document.querySelector(".textarea");
-  var test=digit.test(buttonClicked);
   console.log(buttonClicked);
-  if (test==true) {
+  if (buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9 || buttonClicked = "*" || buttonClicked = "-" || buttonClicked = "+" || buttonClicked = "/") {
     textArea.append(buttonClicked);
   } else if (buttonClicked == "clear") {
     textArea.innerHTML = null;
