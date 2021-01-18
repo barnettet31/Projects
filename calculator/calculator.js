85b28c0088deb1cd49b65774150ba437f4252ffe
Travis <barnette.travis31@gmail.com>
2021-01-17 19:27:47 -0500

    reduced the if then statements by combining the operators with the numbers

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,18 +1,19 @@
 var buttonClicked = null;
-var textAreaValue=0;
+var textAreaValue = 0;
 
 document.querySelector(".button-div").addEventListener("click", function(event) {
   buttonClicked = event.target.value;
   var textArea = document.querySelector(".textarea");
   console.log(buttonClicked);
-  if (buttonClicked == 0 || buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9) {
+  if (buttonClicked == 0 || buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9 || buttonClicked == "+" || buttonClicked == "/" || buttonClicked == "-" || buttonClicked == "*") {
     textArea.append(buttonClicked);
   } else if (buttonClicked == "clear") {
-    textArea.innerHTML= null;
-  } else if(buttonClicked=="+"||buttonClicked=="/"||buttonClicked=="-"||buttonClicked=="*"){
-    textArea.append(buttonClicked);}else if(buttonClicked=="="){var x=document.querySelector(".textarea").innerHTML;
-var y=eval(x);
-document.querySelector(".textarea").innerHTML=y;}else {
+    textArea.innerHTML = null;
+  } else if (buttonClicked == "=") {
+    var x = document.querySelector(".textarea").innerHTML;
+    var y = eval(x);
+    document.querySelector(".textarea").innerHTML = y;
+  } else {
     null;
   }
 
