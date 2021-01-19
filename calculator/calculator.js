960539c30720fcb140084cf0193c13ee706d5bae
Travis <barnette.travis31@gmail.com>
2021-01-18 20:08:39 -0500

    Came up with an idea to prevent the user from pressing the operators multiple times

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,29 +1,38 @@
 var buttonClicked = null;
 var textAreaValue = 0;
+var textArea = document.querySelector(".textarea");
 
 //This is a function that detects the button press and appends the inner text of the text-area div based on that input, then if the user clicks on the equal sign, it will run eval on the string.
 document.querySelector(".button-div").addEventListener("click", function(event) {
   buttonClicked = event.target.value;
-  var textArea = document.querySelector(".textarea");
+
   console.log(buttonClicked);
-  if (buttonClicked==0|| buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9 || buttonClicked == "*" || buttonClicked == "-" || buttonClicked == "+" || buttonClicked == "/") {
+  if (buttonClicked == 0 || buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9) {
     textArea.append(buttonClicked);
+  } else if (buttonClicked == "*" || buttonClicked == "-" || buttonClicked == "+" || buttonClicked == "/") {
+    var textAreaLength=textArea.innerText.length-1;
+    var lastItem = textArea.innerText[textAreaLength] ;
+    if (lastItem== "*" || lastItem == "/" || lastItem == "+" || lastItem == "-") {
+      null;
+    } else {
+      textArea.append(buttonClicked);
+    }
   } else if (buttonClicked == "clear") {
     textArea.innerHTML = null;
   } else if (buttonClicked == "=") {
     var x = document.querySelector(".textarea").innerHTML;
     var y = eval(x);
     document.querySelector(".textarea").innerHTML = y;
-  } else if (buttonClicked=="percent"){
+  } else if (buttonClicked == "percent") {
     var x = document.querySelector(".textarea").innerHTML;
-    var y=eval(x)/100;
-    document.querySelector(".textarea").innerHTML=y;
-  }else if(buttonClicked=="sign-change"){
+    var y = eval(x) / 100;
+    document.querySelector(".textarea").innerHTML = y;
+  } else if (buttonClicked == "sign-change") {
     var x = document.querySelector(".textarea").innerHTML;
-    var y=eval(x)*(-1);
-    document.querySelector(".textarea").innerHTML=y;
+    var y = eval(x) * (-1);
+    document.querySelector(".textarea").innerHTML = y;
 
-  }else {
+  } else {
     null;
   }
 
