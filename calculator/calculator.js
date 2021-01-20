92e5acba6d4ec9fc166aa4eb572e248234ada1e4
Travis <barnette.travis31@gmail.com>
2021-01-19 20:51:38 -0500

    added the more button for future tasks and then replaced the sign change with a back space option using the substr method and then added notes to clarify for the future

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,16 +2,16 @@ var buttonClicked = null;
 var textAreaValue = 0;
 var textArea = document.querySelector(".textarea");
 
-//This is a function that detects the button press and appends the inner text of the text-area div based on that input, then if the user clicks on the equal sign, it will run eval on the string.
-document.querySelector(".button-div").addEventListener("click", function(event) {
-  buttonClicked = event.target.value;
 
+//This is a function that detects the button press and appends the inner text of the text-area div based on that input, then if the user clicks on the equal sign, it will run eval on the string.
+document.querySelector(".button-div").addEventListener("click", function(event){
+  buttonClicked = event.target.value;
+  var textAreaLength = textArea.innerText.length - 1;
+  var lastItem = textArea.innerText[textAreaLength];
   console.log(buttonClicked);
   if (buttonClicked == 0 || buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9) {
     textArea.append(buttonClicked);
   } else if (buttonClicked == "*" || buttonClicked == "-" || buttonClicked == "+" || buttonClicked == "/") {
-    var textAreaLength = textArea.innerText.length - 1;
-    var lastItem = textArea.innerText[textAreaLength];
     if (lastItem == "*" || lastItem == "/" || lastItem == "+" || lastItem == "-" || lastItem == null) {
       null;
     } else {
@@ -27,11 +27,10 @@ document.querySelector(".button-div").addEventListener("click", function(event) 
     var x = document.querySelector(".textarea").innerHTML;
     var y = eval(x) / 100;
     document.querySelector(".textarea").innerHTML = y;
-  } else if (buttonClicked == "sign-change") {
-    var x = document.querySelector(".textarea").innerHTML;
-    var y = eval(x) * (-1);
-    document.querySelector(".textarea").innerHTML = y;
-
+  } else if (buttonClicked == "backspace") {
+//This creates a substring x that goes from the start of the text in the display to the number character just before the end, then replaces the current string with that string
+    var x= textArea.innerText.substr(0,textArea.innerText.length-1);
+    textArea.innerText=x;
   } else if (buttonClicked == ".") {
     var textAreaLength = textArea.innerText.length - 1;
     var lastItem = textArea.innerText[textAreaLength];
@@ -46,6 +45,9 @@ document.querySelector(".button-div").addEventListener("click", function(event) 
     null;
   }
 });
-
+//
+// var x = document.querySelector(".textarea").innerHTML;
+// var y = eval(x) * (-1);
+// document.querySelector(".textarea").innerHTML = y;
 
 // || buttonClicked == 1 || buttonClicked == 2 || buttonClicked == 3 || buttonClicked == 4 || buttonClicked == 5 || buttonClicked == 6 || buttonClicked == 7 || buttonClicked == 8 || buttonClicked == 9 || buttonClicked == "+" || buttonClicked == "/" || buttonClicked == "-" || buttonClicked == "*"
