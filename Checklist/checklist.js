50c10e7ba6c669394c31ab468637ee02d5f1c382
Travis <barnette.travis31@gmail.com>
2021-01-24 16:02:31 -0500

    Created the start of the check list

---
+++ Checklist/checklist.js
@@ -0,0 +1,9 @@
+var textAreaValue=0;
+var toDoListAdd=0;
+
+var textArea=document.querySelector("textarea");
+
+
+document.querySelector(".submit").addEventListener("click", function(){
+  textAreaValue=textArea.value;
+})
