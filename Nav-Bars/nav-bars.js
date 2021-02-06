7787ab7d67b8232ddf87b9963ac4b3a7731ae83f
Travis B <barnette.travis31@gmail.com>
2021-02-06 13:56:15 -0500

    working on the slideshow now and fiddling with some css

--- Nav-Bars/nav-bars.js
+++ Nav-Bars/nav-bars.js
@@ -18,4 +18,17 @@ function showMenu() {
 }
 button.addEventListener("click", showMenu);
 
+var slidesArray=[
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpg',
+    'img4.jpg',
+    'img5.jpg',
 
+]
+// function slideshow(){
+
+//     for(var i=0; i<slidesArray.length-1;i++){
+
+//     }
+// }
