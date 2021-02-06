50058e7af0696966c6492625bf1cbec6c77725c2
Travis B <barnette.travis31@gmail.com>
2021-02-06 12:51:20 -0500

    removed initial alert for testing

--- Nav-Bars/nav-bars.js
+++ Nav-Bars/nav-bars.js
@@ -4,9 +4,7 @@ const hiddenMenu=document.querySelector(".hidden-menu");
 
 function showMenu(){
     let buttonText=button.innerHTML;
-    alert(
-        buttonText
-    );
+   
     if(buttonText=="Menu"){
         button.innerHTML="Close";
     }else if (buttonText=="Close"){
