03cce07042e4856102e97c71454af986f9b21c21
Travis B <barnette.travis31@gmail.com>
2021-02-06 12:59:34 -0500

    made it prettier

--- Nav-Bars/nav-bars.js
+++ Nav-Bars/nav-bars.js
@@ -1,22 +1,22 @@
-const button=document.querySelector(".menu-btn");
-const hiddenMenu=document.querySelector(".hidden-menu");
+const button = document.querySelector(".menu-btn");
+const hiddenMenu = document.querySelector(".hidden-menu");
 
 
-function showMenu(){
-    let buttonText=button.innerHTML;
-    if(buttonText=="Menu"){
-        button.innerHTML="Close";
+function showMenu() {
+    let buttonText = button.innerHTML;
+    if (buttonText == "Menu") {
+        button.innerHTML = "Close";
         hiddenMenu.classList.add("flex");
 
-    }else if (buttonText=="Close"){
-        button.innerHTML="Menu";
+    } else if (buttonText == "Close") {
+        button.innerHTML = "Menu";
         hiddenMenu.classList.remove("flex");
     }
-    
-   
+
+
 
 }
 
-button.addEventListener("click" , showMenu);
+button.addEventListener("click", showMenu);
 
 
